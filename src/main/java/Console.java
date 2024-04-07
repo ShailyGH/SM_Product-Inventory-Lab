2ea76df9664fc2033ccec1e88cf25c82014246eb
Shaily Modi <shailykunal@gmail.com>
2024-04-07 13:53:26 -0400

    Section-3 MainClass and ConsolePrint Welcome message completed.

---
+++ src/main/java/Console.java
@@ -0,0 +1,11 @@
+public class Console
+{
+    public static void printWelcome(){
+        System.out.println("\n" +
+                "**************************************************\n" +
+                "***           Welcome and Bienvenue            ***\n" +
+                "***                    to                      ***\n" +
+                "***          ZipCo Inventory Manager           ***\n" +
+                "**************************************************\n\n");
+    }
+}
