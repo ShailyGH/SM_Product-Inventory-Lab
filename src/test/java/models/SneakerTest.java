dec5b801fd0a8eb171aacd86cea2e0d68772cdb5
Shaily Modi <shailykunal@gmail.com>
2024-04-07 12:48:11 -0400

    completed constructor test inside SneakerTest

--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -95,5 +95,31 @@ public class SneakerTest
         Assert.assertEquals(expected, testSneaker.getPrice(), 0.01);
     }
 
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedId = 6;
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennnis";
+        float expectedSize = 10.5f;
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+
+        // (3)
+        Sneaker testSneaker = new Sneaker(expectedId, expectedName, expectedBrand,
+                expectedSport, expectedSize, expectedQty,expectedPrice);
+
+        // (4)
+        Assert.assertEquals(expectedId, testSneaker.getId());
+        Assert.assertEquals(expectedName, testSneaker.getName());
+        Assert.assertEquals(expectedBrand, testSneaker.getBrand());
+        Assert.assertEquals(expectedSport, testSneaker.getSport());
+        Assert.assertEquals(expectedSize, testSneaker.getSize(), 0.01);
+        Assert.assertEquals(expectedQty, testSneaker.getQty());
+        Assert.assertEquals(expectedPrice, testSneaker.getPrice(), 0.01);
+    }
+
 }
 
