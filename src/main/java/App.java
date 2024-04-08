5005ec45214e216ea096acc4215db2c79c8b8066
Shaily Modi <shailykunal@gmail.com>
2024-04-07 20:00:13 -0400

    completed part 3 menu selection function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,5 +13,29 @@ public class App
         // application logic here
         // call methods to take user input and interface with services
         Console.printWelcome();
+        displayMenu();
+    }
+
+    public void displayMenu()
+    {
+        String[] menuItems = new String[]{"Add product", "View product", "Update product", "Delete product", "Reports", "Exit"};
+
+        int choice = Console.getInput("Main Menu", menuItems);
+
+        switch (choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
     }
 }
--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,3 +1,7 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.Scanner;
+
 public class Console
 {
     public static void printWelcome(){
@@ -8,4 +12,38 @@ public class Console
                 "***          ZipCo Inventory Manager           ***\n" +
                 "**************************************************\n\n");
     }
+
+    public static int getInput(String header, String[] options)
+    {
+        String printOptions = header + "\n";
+
+        for (int i = 0; i < options.length; i++){
+            printOptions += String.format("%d. %s", i+1, options[i]) + "\n";
+        }
+
+        System.out.println(printOptions);
+
+        return Console.getSelection(options.length);
+    }
+
+    public static Integer getSelection(int maxChoices) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (true)
+        {
+            if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= maxChoices)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Please choose a number between between 1 and " + maxChoices);
+                input = scanner.nextLine();
+            }
+        }
+        return Integer.valueOf(input);
+    }
+
+
 }
